858ec9b3f08de0d3854b28be81061fd50fca1bdc
AdilsonMujinga <adilsonmujinga22@gmail.com>
2025-04-21 20:37:46 +0100

    alterado

--- produto/models.py
+++ produto/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 class Clientes(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
     telefone = models.CharField(max_length=20)
-
+    endereco= models.EmailField()
     def __str__(self):
         return self.nome
 
 class Produtos(models.Model):
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.PositiveIntegerField()
+    origem = models.CharField(max_length=50)
+    destino = models.CharField(max_length=50)
+    tipo_produto = models.CharField(max_length=100)
+    quantidade = models.DecimalField(max_digits=10, decimal_places=2)
+    total_a_pagar = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nome
@@ -19,7 +21,9 @@ class Produtos(models.Model):
 class Funcionarios(models.Model):
     nome = models.CharField(max_length=100)
     cargo = models.CharField(max_length=50)
-    data_contratacao = models.DateField()
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=30)
+    
 
     def __str__(self):
         return self.nome
