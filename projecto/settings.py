c48a497e38bd8558eac53f4483ad6e75d8dc8a8d
AdilsonMujinga <adilsonmujinga22@gmail.com>
2025-04-21 17:55:53 +0100

    Atualizai

--- projecto/settings.py
+++ projecto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
